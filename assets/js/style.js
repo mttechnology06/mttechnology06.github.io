438839cd15b6163d4865b9faead2ce198ff0f2e8
Your Name <you@example.com>
2025-02-05 16:30:41 +0700

    version1.3

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,13 +1,16 @@
 
 const navLinks = document.querySelectorAll('.nav-link');
+const container = document.querySelectorAll(".webpage")
+container[0].classList.add("active"); 
 
-
-navLinks.forEach(link => {
-  link.addEventListener('click', function() {
+navLinks.forEach((link,index) => {
+  link.addEventListener('click', () => {  
     // Xóa lớp 'active' khỏi tất cả các nav link
-    navLinks.forEach(item => item.classList.remove('active'));
-
+     navLinks.forEach(item => item.classList.remove('active'));
+     container.forEach(item => item.classList.remove('active')); 
+ 
     // Thêm lớp 'active' vào nav link đã được click
-    this.classList.add('active');
+    navLinks[index].classList.add('active');
+    container[index].classList.add("active"); 
   });
 });
